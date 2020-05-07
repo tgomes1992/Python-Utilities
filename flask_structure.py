f81ace30f9b4eb8fcb4d0af90dc9e9b1ac23ff61
Your Name <tt7.thiago@gmail.com>
2020-05-07 11:36:29 -0300

    Arquivo para a criação de toda a estrutura e arquivos utilizados em projetos flask

---
+++ flask_structure.py
@@ -0,0 +1,38 @@
+import os
+
+#arquivo para criar projetos em flask 
+
+#zeradosC:\
+#functions
+
+def changedir():
+    path = input("Digite o diretório que deseja criar o projeto: ")
+    os.chdir(path)
+
+
+def makefolders():
+    os.mkdir('static')
+    os.mkdir('templates')
+
+
+def create_app():
+    app = open("app.py","w")
+    line = ["import os\n","from flask import Flask,",
+            "render_template,request,redirect,url_for,jsonify\n",
+            "from flask_sqlalchemy import SQLAlchemy\n",
+            "import sys\n","app = Flask(__name__)\n",
+            "#create the app config, including the database data\n",
+            "#templates folder for html, static for css an js files\n",
+            "migrate = Migrate(app, db)\n"]
+    for lines in line:
+        app.write(lines)
+
+def main():
+   changedir()
+   makefolders()
+   create_app()
+
+
+if __name__ == "__main__":
+    main()
+
