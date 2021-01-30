33d759f9d6efdb113c0ebe4e3e97da8f3c8147dc
Thiago Gomes <tt7.thiago@gmail.com>
2021-01-29 23:10:11 -0300

    Mudanças em folder_organizer

--- os_utilities/folder_organizer.py
+++ os_utilities/folder_organizer.py
@@ -3,30 +3,26 @@ import shutil
 
 
 
-folder = os.getcwd()
-
-file = os.listdir(folder)
-
-filetypes = []
-
-files = []
-
-for i in file:
-    if os.path.isfile(folder + "/"+i):
-        files.append(i)    
-
-for i in files:
-    if i.split('.')[-1]  not in filetypes:
-        filetypes.append(i.split('.')[-1])
-
-for i in filetypes:
-    os.mkdir(i)
-
-for i in files:
-    shutil.move(folder + "/" + i , folder + "/" + i.split(".")[-1] )
+def main():    
+    folder = os.getcwd()
+    file = os.listdir(folder)
+    filetypes = []
+    files = []
+    for i in file:
+        if os.path.isfile(folder + "/"+i):
+            files.append(i)    
+    for i in files:
+        if i.split('.')[-1]  not in filetypes:
+            filetypes.append(i.split('.')[-1])
+    for i in filetypes:
+        os.mkdir(i)
+    for i in files:
+        shutil.move(folder + "/" + i , folder + "/" + i.split(".")[-1] )
+return "DONE"
 
 
 
 
 
-
+if __name__ == "__main__":
+    main()
